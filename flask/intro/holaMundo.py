b028fbfb64470a766c1573e27ca5e6e258a57e96
Cristóbal <cristobalcsc@gmail.com>
2022-01-03 20:27:36 +0100

    flask

--- flask/intro/holaMundo.py
+++ flask/intro/holaMundo.py
@@ -6,9 +6,20 @@
 # sudo apt install python3-flask
 # flask run
 
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
+
+import mysql.connector
+
+midb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='root',
+    database='pruebapython'
+)
+
+cursor = midb.cursor(dictionary = True)
 
 @app.route('/')
 def index():
@@ -24,4 +35,22 @@ def lala(post_id):
 
 @app.route('/lele')
 def lele():
-    return 'lele'
+    cursor.execute('select * from usuario')
+    usuarios = cursor.fetchall()
+    return render_template('lele.html', usuarios=usuarios)
+
+@app.route('/home', methods = ['GET'])
+def home():
+    return render_template('home.html', mensaje='Hola mundo')
+
+@app.route('/crear', methods = ['GET', 'POST'])
+def crear():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        sql = 'insert into usuario(username, email) values(%s, %s)'
+        values = (username, email)
+        cursor.execute(sql, values)
+        midb.commit()
+        return redirect(url_for('lele'))
+    return render_template('crear.html')
