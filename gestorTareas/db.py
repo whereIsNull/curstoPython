fb68db61a26936b9702b8e4298a1e6c83b110a70
Cristóbal <cristobalcsc@gmail.com>
2022-04-14 18:45:50 +0200

    gestorTareas

---
+++ gestorTareas/db.py
@@ -0,0 +1,88 @@
+from tkinter import *
+import sqlite3
+
+root = Tk()
+root.title('Hola mundo: todo list')
+root.geometry('500x500')
+
+conn = sqlite3.connect('todo.db')
+
+c = conn.cursor()
+
+c.execute("""
+    CREATE TABLE IF NOT EXISTS todo(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        description TEXT NOT NULL,
+        completed BOOLEAN NOT NULL
+    );
+""")
+
+conn.commit()
+
+def remove(id):
+    def _remove():
+        c.execute("DELETE FROM todo WHERE id = ?", (id, ))
+        conn.commit()
+        render_todos()
+    return _remove
+
+def complete(id):
+    def _complete():
+        todo = c.execute("SELECT * FROM todo WHERE id = ?", (id, )).fetchone()
+        c.execute("UPDATE todo SET completed = ? WHERE id = ?", (not todo[3], id))
+        conn.commit()
+    return _complete # devolvemos una función, es como encapsularlo en un lambda pero devolviendo 
+                     # la función con el parámetro iterado en vez de coger el último (técnica llamada currying)
+
+def render_todos():
+    rows = c.execute("""
+        SELECT * FROM todo
+    """).fetchall()
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+    for i in range(0, len(rows)):
+        id = rows[i][0]
+        completed = rows[i][3]
+        description = rows[i][2]
+        color = '#555555' if completed else '#ffffff'
+        l = Checkbutton(frame, text=description, fg=color, width=42, anchor='w', command=complete(id))
+        l.grid(row=i, column=0, sticky='w')
+        btn = Button(frame, text='Eliminar', command=remove(id))
+        btn.grid(row=i, column=1)
+        l.select() if completed else l.deselect()
+
+
+def addTodo():
+    todo = e.get()
+    if todo: 
+        c.execute("""
+            INSERT INTO todo (description, completed) VALUES (?, ?)
+        """, (todo, False))
+        conn.commit()
+        e.delete(0, 'end')
+        render_todos()
+    else: 
+        pass
+
+l = Label(root, text='Tarea')
+l.grid(row=0, column=0)
+
+e = Entry(root, width=40)
+e.grid(row=0, column=1)
+
+btn = Button(root, text='Agregar', command=addTodo)
+btn.grid(row=0, column=2)
+
+frame = LabelFrame(root, text='Mis tareas', padx=5, pady=5)
+frame.grid(row=1, column=0, columnspan=3, sticky='nswe', padx=5)
+
+e.focus() #pone el focus en este elemento 
+
+root.bind('<Return>', lambda x: addTodo())
+
+render_todos()
+
+root.mainloop()
